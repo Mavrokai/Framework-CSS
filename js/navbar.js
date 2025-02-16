57992fcb18762ea21e290d2f91d1de27edda67d3
mavrokai <mavrokai.valentin.verscheure@epitech.eu>
2025-02-16 23:27:39 +0100

    creation du js avec le contenu

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+const navbarToggle = document.querySelector(".navbar-toggle");
+const navbarNav = document.querySelector(".navbar-nav");
+
+navbarToggle.addEventListener("click", () => {
+    navbarNav.classList.toggle("active");
+});
